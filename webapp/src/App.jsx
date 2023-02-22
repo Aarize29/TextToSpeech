ec0d60025047479910db17bb2ca21fba35d83fbe
lucAsC87 <lucas2187@gmail.com>
2023-02-22 19:34:14 +0100

    [added dark mode]

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
-import './App.css'
-import Speech from './component/Speech'
-import Navbar from './component/Navbar'
-import Home from './component/Home'
-import About from './component/About'
-import Text from './component/Text'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Speech from "./component/Speech";
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
+import About from "./component/About";
+import Text from "./component/Text";
 function App() {
- 
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  function toggleDarkMode() {
+    setDarkMode((prevMode) => !prevMode);
+  }
+
   return (
     <Router>
-    <div className="App">
-
-       <Navbar/>
-       <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/speech' element={<Speech/>}/>
-          <Route path='/text' element={<Text/>}/>
-          <Route path='/about' element={<About/>}/>
-          
-       </Routes>
-        
-        
+      <div className="App">
+        <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+        <Routes>
+          <Route path="/" element={<Home darkMode={darkMode} />} />
+          <Route path="/speech" element={<Speech darkMode={darkMode} />} />
+          <Route path="/text" element={<Text darkMode={darkMode} />} />
+          <Route path="/about" element={<About darkMode={darkMode} />} />
+        </Routes>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- webapp/src/component/Navbar.jsx
+++ webapp/src/component/Navbar.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import './Navbar.css'
-import mylogo from '../assets/mylogo.jpeg'
-import {MdDarkMode} from 'react-icons/md'
-import {Link} from 'react-router-dom'
-const Navbar = () => {
+import React from "react";
+import "./Navbar.css";
+import mylogo from "../assets/mylogo.png";
+import { MdDarkMode } from "react-icons/md";
+import { Link } from "react-router-dom";
+const Navbar = (props) => {
   return (
-    <div className="header">
-    <div className='nav'>
-      <Link to="/"><img className='logo' src={mylogo} alt="" /></Link>
-      <div className="navigation">
-       <Link to="/"> <h2>Home</h2></Link>
-       <Link to="/about"> <h2>About</h2></Link>
-         <h2><MdDarkMode/></h2>
+    <div className={props.darkMode ? "darkHeader" : "header"}>
+      <div className="nav">
+        <Link to="/">
+          <img className="logo" src={mylogo} alt="" />
+        </Link>
+        <div className={props.darkMode ? "navigationDark" : "navigation"}>
+          <Link to="/">
+            {" "}
+            <h2>Home</h2>
+          </Link>
+          <Link to="/about">
+            {" "}
+            <h2>About</h2>
+          </Link>
+          <h2>
+            <MdDarkMode onClick={props.toggleDarkMode} />
+          </h2>
+        </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
